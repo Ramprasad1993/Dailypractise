524d87091677b11fc98668747b7f4f21b270bdc2
Ramprasad <ramprasadalladiqa@gmail.com>
2024-07-04 21:39:09 +0530

    config settings

---
+++ learning.js
@@ -0,0 +1,102 @@
+function login()
+{
+    var age =5;
+    var loginname;
+    loginname="Ramprasad"
+    let usercount = 5;
+    let message = `Hi am rk ${usercount} items test`;
+    console.log(message);
+    console.log("Hi am  "+loginname+"  my age is  "+age+"  Thank you");
+    console.log("My %d age is",age,)
+}
+//login()
+
+
+function symbol111()
+{
+    
+    const x = Symbol('Ramprasad');
+    console.log(x.description);
+}
+//symbol111();
+
+
+function student(){
+    let a=10;
+    let b=5;
+    let c;
+let studentifo;
+let parentinfo;
+studentifo={
+    name:'Rp',
+    class:'2',
+    mobilenumber:989898989898,
+    email:'ram@gmail.com'
+},
+parentinfo=
+{
+    fathername:"prasad",
+    mothername:'mother'
+}
+//console.log(studentifo,parentinfo);
+// c=console.log(a<b);
+ return c=a<b;
+}
+console.log(student());
+//this fucntion is get student info
+function testlogic()
+{
+    let d=1;
+    let e=2;
+    return d>e;
+
+}
+//console.log(testlogic())
+
+
+function equaltest()
+{
+    let a=2;
+    let b=Number('2');
+
+    console.log(a===b);
+
+}
+//equaltest();
+
+
+function conidtions()
+{
+
+
+
+    // if(ramage>35)
+    //     {
+    //         console.log("Ram is just lessthan ")
+    //     }
+    //     else
+    //     {
+    //         console.log("Ram is greter than 32 ")
+    //     }
+
+    if(number>33)
+        {
+            console.log("if statement is executed")
+        }
+        console.log("out side code executed")
+}
+//conidtions();
+
+function loops()
+{
+ let nickname='Ramprasad';
+ let nicksize=nickname.length;
+ console.log(nicksize)
+    for( let nicksizeeee=0;nicksizeeee<nicksize;nicksizeeee++)
+        {
+           console.log(nickname[nicksizeeee])
+           
+        }
+         
+}
+loops();
